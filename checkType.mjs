31aba8b65f9fde165ed8afeee96a5fc42d34f78f
dunz <zergcake@gmail.com>
2019-12-12 11:04:21 +0900

    add type:module in package.json

--- checkType.mjs
+++ checkType.mjs
@@ -3,21 +3,3 @@ export const checkType = (param) => {
     console.log(result);
     return result;
 };
-
-checkType(1);
-checkType('asdf');
-checkType(true);
-checkType(Symbol(1));
-checkType(BigInt(12312312312312312312312321321312312312312312312312321312312388888));
-checkType([1, 2, 3]);
-checkType(null);
-checkType(undefined);
-checkType(() => {});
-checkType(new Promise(() => {}));
-
-/**
- * invalid in Node
- */
-// checkType(document.body);
-// checkType(document);
-// checkType(document.createElement('div'));
